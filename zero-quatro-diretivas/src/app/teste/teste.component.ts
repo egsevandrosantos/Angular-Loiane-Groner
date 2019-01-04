f6ea9f08273513349ab94f2b6210c0253404c2dc
Evandro Gabriel dos Santos <egs.evandro.santos@gmail.com>
2019-01-04 15:19:37 -0200

    Aula 020

--- zero-quatro-diretivas/src/app/teste/teste.component.ts
+++ zero-quatro-diretivas/src/app/teste/teste.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teste.component.scss']
 })
 export class TesteComponent implements OnInit {
+  minhaVariavel: string;
 
   constructor() { }
 
